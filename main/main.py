c37e8ab14357c7fcc2a20b16ad5c5c9b8309f043
eren23 <erenak1996@gmail.com>
2020-12-23 20:34:20 +0100

    problem remains migrating process checked

--- main/main.py
+++ main/main.py
@@ -10,17 +10,24 @@ CORS(app)
 
 db = SQLAlchemy(app)
 
+
 class Product(db.Model):
+    id: int
+    title: str
+    image: str
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
 
-class ProductUser(db.Model):
-    id= db.Column(db.Integer, primary_key=True)
-    user_id=db.Column(db.Integer)
-    product_id=db.Column(db.Integer)
 
-    UniqueConstraint('user_id','product_id', name='user_product_unique')
+
+class ProductUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    product_id = db.Column(db.Integer)
+
+    UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
 @app.route('/')
 def index():
